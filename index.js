d3b8b23b38cb0a2f6d5ee85845d73fb1bb9fe2f4
Hari Kiran <113755124+harikiran5184@users.noreply.github.com>
2023-11-22 21:01:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ app.use(cors())
 app.get('/',(req,res)=>{
     res.json({"data":cities.data})
 })
+app.get('/search', (req, res) => {
+    const s = req.query.data;
+    const matchingCities = cities.data.filter(city => city.includes(s));
 
+    res.json({ data:matchingCities });
+});
 app.listen(5000,()=>{console.log("server is running")})
